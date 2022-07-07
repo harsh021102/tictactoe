1a438bf5df710543f43eabf27bf751335843d7a2
harsh021102 <harsh021102@gmail.com>
2022-07-07 22:19:52 +0530

    history added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Board from './components/Board';
+import History from './components/History';
 import { calculateWinner } from './helpers';
 import "./styles/root.scss"
 function App() {
@@ -31,11 +32,18 @@ function App() {
         })
         setCurrentMove(prev=> prev+1)
     }
+
+    const moveTo = (move) => {
+      setCurrentMove(move)
+    }
+
+
   return (
     <div className='app'>
       <h1>TIC TAC TOE</h1>
       <h1>{message}</h1>
       <Board board={current.board} handleSquareClick={handleSquareClick}/>
+      <History history={history} moveTo={moveTo} currentMove={currentMove}/>
     </div>
   );
 }
